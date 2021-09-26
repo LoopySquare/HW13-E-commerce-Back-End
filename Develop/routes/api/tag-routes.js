1e8bdfca7b50cdcae42e72b7228287abbd5c92f8
LoopySquare <tjobin1@gmail.com>
2021-09-25 22:13:49 -0600

    updated model requirements and tag routes

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -7,31 +7,86 @@ router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
   try {
-    // find all categories and perform a JOIN to include all associated products
+    // find all tags and perform a JOIN to include all associated products
     const tags = await Tag.findAll({
-      include: [{ model: Product, ProductTag}]
+      include: [{ model: Product }]
     });
-    res.status(200).json(categories);
+    res.status(200).json(tags);
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    // find specific tag and perform a JOIN to include all associated products
+    const tags = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product}]
+    });
+
+    if (!tags) {
+      res.status(404).json({ message: 'No tag found with this id!' });
+      return;
+    };
+
+    res.status(200).json(tags);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    const tags = await Tag.create({
+      tag_name: req.body.tag_name
+    });
+
+    res.status(200).json(tags);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const tags = await Tag.update({
+      tag_name: req.body.tag_name
+    },
+    {
+      where: { id: req.params.id }
+    });
+
+    if (!tags) {
+      res.status(404).json({ message: 'No tag found with this id!' });
+      return;
+    };
+
+    res.status(200).json({ message: 'Tag name updated' });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const tags = await Tag.destroy({
+      where: { id: req.params.id }
+    });
+
+    if (!tags) {
+      res.status(404).json({ message: 'No tag found with this id!' });
+      return;
+    };
+
+    res.status(200).json({ message: 'Tag deleted' });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
